7b491558fa5a60939175f76376691e064d2ad8df
khayashi2 <35716834+khayashi2@users.noreply.github.com>
2019-05-02 23:08:50 -0700

    Add files via upload

--- GraphImplementation.java
+++ GraphImplementation.java
@@ -10,8 +10,8 @@ public class GraphImplementation implements Graph{
 	private int sizeV;
 
 	/**
-	 * Constructor makes 
-	 * @param v
+	 * Constructor creates an adjacent matrices and creates a vertices depending on the parameter. Then initialize all verts to its corresponding index
+	 * @param v the amount of nodes/vertices
 	 */
 	public GraphImplementation(int v) {
 		edges = new int[v][v];
@@ -22,12 +22,15 @@ public class GraphImplementation implements Graph{
 		sizeV = v;
 	}
 
-	@Override
+	/**
+	 * Creates an edge between two vertex 
+	 * @param v1 start of and edge
+	 * @param v2 the end of an edge
+	 */
 	public void addEdge(int v1, int v2) {
 		//if vertices are out of bounds of array
 		if(v1 >= edges.length || v2 >= edges.length)
 			throw new ArrayIndexOutOfBoundsException("Vertex out of bounds");
-		
 		//if vertices are already an edge
 		else if(edges[v1][v2] == 1) {
 			System.out.println("This edge already exist between the two");
@@ -37,16 +40,23 @@ public class GraphImplementation implements Graph{
 			edges[v1][v2] = 1;
 	}
 
-	@Override
+	/**
+	 * sorts the array using topological sorting
+	 * @return returns an ordered list
+	 */
 	public List<Integer> topologicalSort() {
+		//create a stack
 		Stack s = new Stack();
+		//v for vertex
 		int v;
-		//count amount of rows has arrays
+		//incident of the amount of edges existing in a row
 		int[] incident = new int[sizeV];
 		Arrays.fill(incident, 0);
 		
+		//List to store the ordered integers
 		List<Integer> topSort = new ArrayList<Integer>();
 		
+		//go through the adjacent matrix and increment incident if an edge exist
 		for(int start = 0; start < sizeV; start++) {
 			for(int end = 0; end < sizeV; end++)
 				if(edges[start][end] == 1)
@@ -54,15 +64,22 @@ public class GraphImplementation implements Graph{
 			//inner for
 		}//outer for
 		
+		
+		//go through the incident array and if it is a zero, add the index to the stack
 		for(v = 0; v < sizeV; v++) {
 			if(incident[v] == 0)
 				s.push(v);
 		}
 
-		
+		//keep doing until the stack is empty
 		while(!s.empty()) {
+			//pop the stack and store it
 			v = (int) s.pop();
+			
+			//add the popped value to the sorted list
 			topSort.add(0, v);
+			
+			//go through the incident array and decrement. if decrement value is 0, then add that index to the stack
 			for(int column  = 0; column < sizeV; column++) {
 				if(--incident[column] == 0) {
 					s.push(column);	
@@ -72,7 +89,11 @@ public class GraphImplementation implements Graph{
 		return topSort;
 	}
 
-	@Override
+	/**
+	 * Finds the neighbors of a particular neighbor
+	 * @param the vertex to finds its neighbors
+	 * @return returns an array of the vertex's neighbors
+	 */
 	public int[] neighbors(int vertex) {
 		List<Integer> neighborsL = new ArrayList<Integer>();
 		for(int i = 0; i < sizeV; i++) {
