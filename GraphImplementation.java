1eddfa76d8a1d891bcf613f1be376c9566acd439
khayashi2 <35716834+khayashi2@users.noreply.github.com>
2019-05-02 23:07:21 -0700

    Add files via upload

--- GraphImplementation.java
+++ GraphImplementation.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Stack;
 
 
 public class GraphImplementation implements Graph{
@@ -8,6 +9,10 @@ public class GraphImplementation implements Graph{
 	private int[] verts;
 	private int sizeV;
 
+	/**
+	 * Constructor makes 
+	 * @param v
+	 */
 	public GraphImplementation(int v) {
 		edges = new int[v][v];
 		verts = new int[v];
@@ -34,6 +39,8 @@ public class GraphImplementation implements Graph{
 
 	@Override
 	public List<Integer> topologicalSort() {
+		Stack s = new Stack();
+		int v;
 		//count amount of rows has arrays
 		int[] incident = new int[sizeV];
 		Arrays.fill(incident, 0);
@@ -41,14 +48,27 @@ public class GraphImplementation implements Graph{
 		List<Integer> topSort = new ArrayList<Integer>();
 		
 		for(int start = 0; start < sizeV; start++) {
-			for(int end = 0; start < sizeV; start++)
+			for(int end = 0; end < sizeV; end++)
 				if(edges[start][end] == 1)
 					incident[start]++;
 			//inner for
 		}//outer for
 		
+		for(v = 0; v < sizeV; v++) {
+			if(incident[v] == 0)
+				s.push(v);
+		}
+
 		
-		
+		while(!s.empty()) {
+			v = (int) s.pop();
+			topSort.add(0, v);
+			for(int column  = 0; column < sizeV; column++) {
+				if(--incident[column] == 0) {
+					s.push(column);	
+				}
+			}
+		}
 		return topSort;
 	}
 
